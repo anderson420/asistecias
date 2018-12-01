cf458ee773f12dfc5cf47c4128b65a88db9a6c0e
anderson420 <ande42061@gmail.com>
2018-12-01 14:24:46 -0500

    Update asistencia.sql

--- asistencia.sql
+++ asistencia.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-12-2018 a las 01:46:14
--- Versión del servidor: 10.1.35-MariaDB
--- Versión de PHP: 7.2.9
+-- Tiempo de generación: 01-12-2018 a las 20:24:24
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -34,15 +34,6 @@ CREATE TABLE `cursos` (
   `creditos` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Volcado de datos para la tabla `cursos`
---
-
-INSERT INTO `cursos` (`idcursos`, `nombre`, `creditos`) VALUES
-(1, 'Programación para la web', '4'),
-(2, 'Ingeniería de software', '4'),
-(3, 'Ingeniería de software II', '3');
-
 -- --------------------------------------------------------
 
 --
@@ -53,25 +44,6 @@ CREATE TABLE `cursos_has_estudiantes` (
   `cursos_idcursos` int(11) NOT NULL,
   `estudiantes_idestudiantes` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `cursos_has_estudiantes`
---
-
-INSERT INTO `cursos_has_estudiantes` (`cursos_idcursos`, `estudiantes_idestudiantes`) VALUES
-(1, 2009214040),
-(1, 2015214132),
-(1, 2016114057),
-(1, 2016114126),
-(1, 2016214076),
-(2, 2014114054),
-(2, 2016214041),
-(2, 2016214069),
-(2, 2016214082),
-(3, 2015214132),
-(3, 2016114057),
-(3, 2016114126),
-(3, 2016214076);
 
 -- --------------------------------------------------------
 
@@ -85,34 +57,58 @@ CREATE TABLE `estudiantes` (
   `segundoNombre` varchar(45) DEFAULT NULL,
   `primerApellido` varchar(45) NOT NULL,
   `segundoApellido` varchar(45) DEFAULT NULL,
-  `email` varchar(45) NOT NULL
+  `correo` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Volcado de datos para la tabla `estudiantes`
+-- Estructura de tabla para la tabla `lista_asistencia`
 --
 
-INSERT INTO `estudiantes` (`idestudiantes`, `primerNombre`, `segundoNombre`, `primerApellido`, `segundoApellido`, `email`) VALUES
-(2008114147, 'Manuel', 'Rodrigo', 'Valdez', 'Lozano', 'manuelvaldezrl@miunimagdalena.edu.co'),
-(2009214040, 'Daniel', NULL, 'Sabogal', NULL, 'danielsabogals@miunimagdalena.edu.co'),
-(2013214048, 'Kevin', 'Jose', 'Esquea', 'Estrada', 'kevinesqueaje@miunimagdalena.edu.co'),
-(2013214057, 'Michael', 'Ferneein', 'Garzon', 'Rodriguez', 'michaelgarzonfr@miunimagdalena.edu.co'),
-(2013214092, 'Misael', 'Duban', 'Ortiz', 'Palomino', 'misaelortizdp@miunimagdalena.edu.co'),
-(2014114054, 'Karul', 'Andres', 'Ramirez', 'Castillo', 'karulramirezac@miunimagdalena.edu.co'),
-(2014114102, 'Tania', 'Margarita', 'Goenaga', 'Raigosa', 'taniagoenagamr@miunimagdalena.edu.co'),
-(2014114114, 'Joel', 'David', 'Alvarez', 'Ayola', 'joelalvarezda@miunimagdalena.edu.co'),
-(2014114132, 'Julian', 'Andres', 'Rueda', 'Pacheco', 'julianruedaap@miunimagdalena.edu.co'),
-(2015214050, 'Yoicer', 'Dilan', 'Galvis', 'Amador', 'yoicergalvisda@miunimagdalena.edu.co'),
-(2015214080, 'Keiner', 'Rafael', 'Molina', 'Lemus', 'keinermoliarl@miunimagdalena.edu.co'),
-(2015214132, 'Hallel', 'Sarid', 'Vargas', 'Picon', 'hallelvargassp@miunimagdalena.edu.co'),
-(2016114057, 'Geraldine', 'De Jesus', 'Granados', 'Buendia', 'geraldinegranadosdb@miunimagdalena.edu.co'),
-(2016114126, 'Deiler', 'Galdino', 'Santana', 'Buendia', 'deilersantanagb@miunimagdalena.edu.co'),
-(2016214037, 'Jose', 'David', 'Cantillo', 'De la Cruz', 'josecantillodd@miunimagdalena.edu.co'),
-(2016214041, 'Yamid', 'Jose', 'Rodriguez', 'Rodriguez', 'yamidrodriguezjr@miunimagdalena.edu.co'),
-(2016214052, 'Kevin', 'Johan', 'Velasquez', 'Hernandez', 'kevinvelasquezjh@miunimagdalena.edu.co'),
-(2016214069, 'Jose', 'Azadi', 'Restrepo', 'Lopez', 'joserestrepoal@miunimagdalena.edu.co'),
-(2016214076, 'Christian', 'David', 'Rodriguez', 'Navia', 'christianrodriguezdn@miunimagdalena.edu.co'),
-(2016214082, 'Rodrigo', 'Jose', 'Venegas', 'Vides', 'rodrigovenegasjv@miunimagdalena.edu.co');
+CREATE TABLE `lista_asistencia` (
+  `id_lista_asistencia` int(11) NOT NULL,
+  `tema` varchar(45) NOT NULL,
+  `cursos_idcursos` int(11) NOT NULL,
+  `fecha_hora` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lista_asistencia_estudiantes`
+--
+
+CREATE TABLE `lista_asistencia_estudiantes` (
+  `estudiantes_idestudiantes` int(11) NOT NULL,
+  `lista_asistencia_id_lista_asistencia` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensajes`
+--
+
+CREATE TABLE `mensajes` (
+  `idmensajes` int(11) NOT NULL,
+  `mensaje` varchar(200) DEFAULT NULL,
+  `profesores_idprofesores` int(11) NOT NULL,
+  `cursos_idcursos` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensaje_estudiante`
+--
+
+CREATE TABLE `mensaje_estudiante` (
+  `idmensaje` int(11) NOT NULL,
+  `mensaje` int(11) NOT NULL,
+  `estudiantes_idestudiantes` int(11) NOT NULL,
+  `cursos_idcursos` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -131,16 +127,8 @@ CREATE TABLE `profesores` (
   `telefono` varchar(45) NOT NULL,
   `dia_nacimiento` varchar(45) NOT NULL,
   `mes_nacimiento` varchar(45) NOT NULL,
-  `anio_nacimiento` varchar(45) NOT NULL,
-  `contrasena` varchar(45) NOT NULL
+  `anio_nacimiento` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `profesores`
---
-
-INSERT INTO `profesores` (`idprofesores`, `primerNombre`, `email`, `primerApellido`, `segundoNombre`, `segundoApellido`, `direccion`, `telefono`, `dia_nacimiento`, `mes_nacimiento`, `anio_nacimiento`, `contrasena`) VALUES
-(12094, 'Johan', 'johanrobles@hotmail.com', 'Robles', NULL, 'Solano', 'Ciénaga - Magdalena', '3008007054', '30', 'marzo', '1976', '12321333');
 
 -- --------------------------------------------------------
 
@@ -154,15 +142,6 @@ CREATE TABLE `profesores_has_cursos` (
   `salon` varchar(45) NOT NULL,
   `hora` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `profesores_has_cursos`
---
-
-INSERT INTO `profesores_has_cursos` (`profesores_idprofesores`, `cursos_idcursos`, `salon`, `hora`) VALUES
-(12094, 1, 'Modelado y Simulación', '8:00am - 12:00pm'),
-(12094, 2, 'Tecnologías de la Información', '8:00am - 12:00pm'),
-(12094, 3, 'Modelado y Simulación', '7:00 - 10:00am');
 
 --
 -- Índices para tablas volcadas
@@ -189,6 +168,36 @@ ALTER TABLE `estudiantes`
   ADD PRIMARY KEY (`idestudiantes`);
 
 --
+-- Indices de la tabla `lista_asistencia`
+--
+ALTER TABLE `lista_asistencia`
+  ADD PRIMARY KEY (`id_lista_asistencia`),
+  ADD KEY `fk_lista_asistencia_cursos1_idx` (`cursos_idcursos`);
+
+--
+-- Indices de la tabla `lista_asistencia_estudiantes`
+--
+ALTER TABLE `lista_asistencia_estudiantes`
+  ADD PRIMARY KEY (`estudiantes_idestudiantes`,`lista_asistencia_id_lista_asistencia`),
+  ADD KEY `fk_lista_asistencia_lista_asistencia1_idx` (`lista_asistencia_id_lista_asistencia`);
+
+--
+-- Indices de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD PRIMARY KEY (`idmensajes`),
+  ADD KEY `fk_mensajes_profesores1_idx` (`profesores_idprofesores`),
+  ADD KEY `fk_mensajes_cursos1_idx` (`cursos_idcursos`);
+
+--
+-- Indices de la tabla `mensaje_estudiante`
+--
+ALTER TABLE `mensaje_estudiante`
+  ADD PRIMARY KEY (`idmensaje`,`mensaje`),
+  ADD KEY `fk_mensaje_estudiante_estudiantes1_idx` (`estudiantes_idestudiantes`),
+  ADD KEY `fk_mensaje_estudiante_cursos1_idx` (`cursos_idcursos`);
+
+--
 -- Indices de la tabla `profesores`
 --
 ALTER TABLE `profesores`
@@ -210,7 +219,19 @@ ALTER TABLE `profesores_has_cursos`
 -- AUTO_INCREMENT de la tabla `cursos`
 --
 ALTER TABLE `cursos`
-  MODIFY `idcursos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idcursos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `lista_asistencia`
+--
+ALTER TABLE `lista_asistencia`
+  MODIFY `id_lista_asistencia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  MODIFY `idmensajes` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -222,6 +243,33 @@ ALTER TABLE `cursos`
 ALTER TABLE `cursos_has_estudiantes`
   ADD CONSTRAINT `fk_cursos_has_estudiantes_cursos1` FOREIGN KEY (`cursos_idcursos`) REFERENCES `cursos` (`idcursos`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_cursos_has_estudiantes_estudiantes1` FOREIGN KEY (`estudiantes_idestudiantes`) REFERENCES `estudiantes` (`idestudiantes`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `lista_asistencia`
+--
+ALTER TABLE `lista_asistencia`
+  ADD CONSTRAINT `fk_lista_asistencia_cursos1` FOREIGN KEY (`cursos_idcursos`) REFERENCES `cursos` (`idcursos`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `lista_asistencia_estudiantes`
+--
+ALTER TABLE `lista_asistencia_estudiantes`
+  ADD CONSTRAINT `fk_lista_asistencia_estudiantes1` FOREIGN KEY (`estudiantes_idestudiantes`) REFERENCES `estudiantes` (`idestudiantes`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_lista_asistencia_lista_asistencia1` FOREIGN KEY (`lista_asistencia_id_lista_asistencia`) REFERENCES `lista_asistencia` (`id_lista_asistencia`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD CONSTRAINT `fk_mensajes_cursos1` FOREIGN KEY (`cursos_idcursos`) REFERENCES `cursos` (`idcursos`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_mensajes_profesores1` FOREIGN KEY (`profesores_idprofesores`) REFERENCES `profesores` (`idprofesores`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `mensaje_estudiante`
+--
+ALTER TABLE `mensaje_estudiante`
+  ADD CONSTRAINT `fk_mensaje_estudiante_cursos1` FOREIGN KEY (`cursos_idcursos`) REFERENCES `cursos` (`idcursos`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_mensaje_estudiante_estudiantes1` FOREIGN KEY (`estudiantes_idestudiantes`) REFERENCES `estudiantes` (`idestudiantes`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `profesores_has_cursos`
