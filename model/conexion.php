3d5bc3b6821766d7a6c4b0a237975bbf67622f77
Koutawars <admikouta@gmail.com>
2018-12-03 01:48:13 -0500

    edit

--- model/conexion.php
+++ model/conexion.php
@@ -6,6 +6,7 @@
 
 			try{
 				$conexion = new PDO('mysql:host=127.0.0.1; dbname=asistencia', 'root', '');
+				//$conexion = new PDO('mysql:host=127.0.0.1; dbname=asistencia', 'root', '');
 				$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				$conexion->exec("SET CHARACTER SET UTF8");
 
